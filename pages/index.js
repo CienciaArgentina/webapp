8ca9f368462cca9fde8b073ece04591e105cfd72
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2019-01-27 21:14:44 -0300

    Implement NextJs and sass

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import "../scss/style.scss";
+
+const index = () => (
+    <div>
+        <p>Hello world</p>
+    </div>
+)
+
+export default index;
--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-
-var app = express()
-
-app.listen(3001, () => {
-  console.log('La aplicación está escuchando en el puerto 3001')
-});
-

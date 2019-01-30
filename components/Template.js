2cec86988e91b6f3bb6ddf148646c67d6b65d7bd
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2019-01-30 16:17:11 -0300

    Add icons

--- components/Template.js
+++ components/Template.js
@@ -1,5 +1,7 @@
 import Header from './Header';
-import {Component} from 'react';
+import { Component } from 'react';
+import Head from 'next/head'
+import Document, { Main, NextScript } from 'next/document';
 
 export default class Page extends Component {
 	state = {
@@ -22,6 +24,53 @@ export default class Page extends Component {
 	render() {
 		return (
 			<div id="app" {...this.props.pageprops}>
+				<Head>
+					<title>{this.props.title?this.props.title+' | ':''}Ciencia Argentina</title>
+					<meta name="og:title" property="og:title" content={
+						this.props.title?this.props.title+' | Ciencia Argentina':'Ciencia Argentina'
+					} />
+					<meta name="description" content={
+						this.props.description ? this.props.description :
+						'Encontrá tu lugar en la ciencia.'
+					} />
+					<meta name="og:description" property="og:description" content={
+						this.props.description ? this.props.description :
+						'Encontrá tu lugar en la ciencia.'
+					} />
+					<meta name="og:type" property="og:type" content="website" />
+					<meta name="og:image" property="og:image" content={
+						this.props.ogimage ?
+						this.props.ogimage :
+						'/static/img/favicon/ogimage.jpg'
+					} />
+					{/* FAVICONS */}
+					<link rel="icon" type="image/x-icon" href="/static/img/favicon/favicon.ico" />
+					<link rel="icon" type="image/png" sizes="192x192"  href="/static/img/favicon/android-icon-192x192.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/static/img/favicon/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="96x96" href="/static/img/favicon/favicon-96x96.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/static/img/favicon/favicon-16x16.png" />
+					{/* apple */}
+					<link rel="apple-touch-icon" sizes="57x57" href="/static/img/favicon/apple-icon-57x57.png" />
+					<link rel="apple-touch-icon" sizes="60x60" href="/static/img/favicon/apple-icon-60x60.png" />
+					<link rel="apple-touch-icon" sizes="72x72" href="/static/img/favicon/apple-icon-72x72.png" />
+					<link rel="apple-touch-icon" sizes="76x76" href="/static/img/favicon/apple-icon-76x76.png" />
+					<link rel="apple-touch-icon" sizes="114x114" href="/static/img/favicon/apple-icon-114x114.png" />
+					<link rel="apple-touch-icon" sizes="120x120" href="/static/img/favicon/apple-icon-120x120.png" />
+					<link rel="apple-touch-icon" sizes="144x144" href="/static/img/favicon/apple-icon-144x144.png" />
+					<link rel="apple-touch-icon" sizes="152x152" href="/static/img/favicon/apple-icon-152x152.png" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/static/img/favicon/apple-icon-180x180.png" />
+					{/* android */}
+					{/* ms */}
+					<meta name="msapplication-TileColor" content="#1b75bb" />
+					<meta name="msapplication-TileImage" content="/static/img/favicon/ms-icon-144x144.png" />
+					<meta name="msapplication-square70x70logo" content="/static/img/favicon/ms-icon-70x70.png" />
+					<meta name="msapplication-square150x150logo" content="/static/img/favicon/ms-icon-150x150.png" />
+					<meta name="msapplication-square310x310logo" content="/static/img/favicon/ms-icon-310x310.png" />
+					<meta name="msapplication-wide310x150logo" content="/static/img/favicon/ms-icon-wide-310x150.png" />
+					{/* theme */}
+					<meta name="theme-color" content="#1b75bb" />
+					{/* END FAVICONS */}
+				</Head>
 				<Header
 					showMenu={this.showMenu}
 					hideMenu={this.hideMenu}
@@ -33,26 +82,11 @@ export default class Page extends Component {
 				</div>
 				<footer>
 					<div>
-						<img src="./static/img/logos/icon-white.svg" />
-						© 2019 Ciencia Argentina
+						<img src="/static/img/logos/icon-white.svg" />
+						Ciencia Argentina
 					</div>
 				</footer>
 			</div>
 		);
 	}
 }
-
-// const Page = (props) => (
-// 	<div id="app">
-// 		<Header  />
-// 		<div id="content">
-// 			{props.children}
-// 		</div>
-// 		<footer>
-// 			<div>
-// 				<img src="./static/img/logos/icon-white.svg" />
-// 				© 2019 Ciencia Argentina
-// 			</div>
-// 		</footer>
-// 	</div>
-// );

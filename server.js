671d3a1a330f310d3ad8add910f929c708cd5e74
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2019-02-23 19:28:23 -0300

    Produccion fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const next = require('next');
 
 const PORT = process.env.PORT || 3000;
-const dev = process.env.NODE_NEV !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 

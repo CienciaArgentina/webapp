2ebd6b4fa07eb1eb5b31b2cdbdd2377b659ca8ba
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2019-08-19 20:42:40 -0300

    Handle undefined errors

--- pages/_error.js
+++ pages/_error.js
@@ -4,31 +4,36 @@ import Page from '../layouts/main/main';
 
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    const statusCode = res ? res.statusCode : (err ? err.statusCode : 404);
     return { statusCode }
   }
 
   render() {
+	  const statusCode = this.props.statusCode;
     return (
 		<Page>
 			{(this.props.message||this.props.title) ?
 				<div className="errorPage">
-					{this.props.statusCode && <p className="errorCode">Error {this.props.statusCode}</p>}
+					{statusCode && <p className="errorCode">Error {statusCode}</p>}
 					<p className="errorName">{this.props.title}</p>
 					<p className="errorDesc">{this.props.message}</p>
 				</div>
 			:
 				<div className="errorPage">
-					{this.props.statusCode==404 && <img src="/static/img/404.svg" />}
-					{this.props.statusCode==500 && <img src="/static/img/500.svg" />}
-					<p className="errorCode">Error {this.props.statusCode}</p>
+					{statusCode==404 && <img src="/static/img/404.svg" />}
+					{(statusCode==500) && <img src="/static/img/500.svg" />}
+					{statusCode!='undefined' ?
+						<p className="errorCode">Error {statusCode}</p>
+					:
+						<p className="errorCode">Algo salió mal...</p>						
+					}
 					<p className="errorName">
-						{this.props.statusCode == 404 && 'Page not found'}
-						{this.props.statusCode == 500 && 'Internal server error'}
+						{statusCode == 404 && 'Page not found'}
+						{statusCode == 500 && 'Internal server error'}
 					</p>
 					<p className="errorDesc">
-						{this.props.statusCode == 404 && 'El diccionario lo define como una página que no se puede encontrar... Que NO se puede encontrar.'}
-						{this.props.statusCode == 500 && 'Se descompone fácilmente.'}
+						{statusCode == 404 && 'El diccionario lo define como una página que no se puede encontrar... Que NO se puede encontrar.'}
+						{statusCode == 500 && 'Se descompone fácilmente.'}
 					</p>
 				</div>
 			}

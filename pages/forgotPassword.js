e788dd5ae03a50023290fdc44dd25a972a008c9f
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2019-11-23 14:49:52 -0300

    Forgot password page

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -1,12 +1,71 @@
 import React from 'react'
 import Page from '../layouts/main/main'
 import { Input } from '../components/Science'
+import { AuthApi } from '../src/api/api'
+import Modal from 'react-modal'
+Modal.setAppElement('#app');
 
 export default class ForgotPassword extends React.Component {
+	state = {
+		loading: false,
+		sended: false,
+		errorForm: false,
+		errorMsg: false,
+		email: ''
+	}
+	onSubmit = e => {
+		e.preventDefault()
+		this.setState(()=>({loading:true}))
+		if(this.state.loading || this.state.error || !this.emailInput.validate().valid || this.state.sended) {
+			return false
+		}
+		AuthApi.sendForgotPassword(this.state.email).then( () => {
+			this.setState(()=>({
+				loading: false,
+				sended: true
+			}))
+		}).catch(err => {
+			if(err.status == 400) {
+				const errorMsg = {
+					EmailNotFound: 'No existe una cuenta con ese correo electrónico.',
+				}[err.data[0].code]
+				this.setState(()=>({
+					loading: false,
+					error: true,
+					errorMsg
+				}))
+			} else {
+				this.setState(()=>({
+					loading: false,
+					error: true,
+					errorMsg: 'Error desconocido.'
+				}))
+			}
+		})
+	}
+	handleChange = e => {
+		const name = e.target.name
+		const val = e.target.value
+		this.setState(()=>({
+			[name]: val
+		}));
+	}
 	render() {
 		return (
-			<Page>
+			<Page loading={this.state.loading}>
 				<div className='pt-6 pb-4'>
+					<Modal
+						isOpen={this.state.error}
+						className='defaultModal --adviceModal --s'
+					>
+						<p>{this.state.errorMsg}</p>
+					</Modal>
+					<Modal
+						isOpen={this.state.sended}
+						className='defaultModal --adviceModal --s'
+					>
+						<p>Te enviamos un correo electrónico a <b>{this.state.email}</b> para recuperar tu cuenta.</p>
+					</Modal>
 					<div className='__logForm'>
 						<h2 className='mb-2'>Recurperar contraseña</h2>
 						<form onSubmit={this.onSubmit}>
@@ -16,10 +75,18 @@ export default class ForgotPassword extends React.Component {
 									required
 									fullWidth
 									label='Correo electrónico'
-									value={''}
+									placeholder="ejemplo@mail.com"
+									onChange={this.handleChange}
+									value={this.state.email}
+									name='email'
+									validation = {[
+										v => /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(v)
+										? true : 'Ingresa un correo válido'
+									]}
+									ref={ (input) => {this.emailInput = input} }
 								/>
 							</div>
-							<button className>Enviar</button>
+							<button>Enviar</button>
 						</form>
 					</div>
 				</div>

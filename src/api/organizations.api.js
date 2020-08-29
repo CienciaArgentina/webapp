70d09b74209f2963d9511452d915a3a8e3404b93
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2020-08-29 17:52:20 -0300

    Not found fix

--- src/api/organizations.api.js
+++ src/api/organizations.api.js
@@ -2,15 +2,15 @@ import axiosInstance from './utils/axiosInstance'
 
 export class OrganizationsApi {
 	static async getOrganization(id) {
-		const response = await axiosInstance.get(`/organizations/${id}`)
-		return response.data
+		const response = await axiosInstance.get(`/organizations/${id}`).catch(e=>{})
+		return response?.data
 	}
 	static async getDepartment(id) {
-		const response = await axiosInstance.get(`/departments/${id}`)
-		return response.data
+		const response = await axiosInstance.get(`/departments/${id}`).catch(e=>{})
+		return response?.data
 	}
 	static async getProject(id) {
-		const response = await axiosInstance.get(`/projects/${id}`)
-		return response.data
+		const response = await axiosInstance.get(`/projects/${id}`).catch(e=>{})
+		return response?.data
 	}
 }

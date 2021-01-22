a92218880660969403dfb5f211c8cec5448ef444
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2021-01-22 17:20:10 -0300

    minor change

--- src/utils/api/users.ts
+++ src/utils/api/users.ts
@@ -17,8 +17,8 @@ interface UserRequest {
 export const login = async (
   userRequest: UserRequest
 ): Promise<LoginResponse> => {
+  const path = '/login';
   const httpRequest = httpClient(cienciaArgentinaHost);
-  const path = 'login';
   const { data } = await httpRequest.post<LoginResponse>(path, userRequest);
   return data;
 };
@@ -26,8 +26,8 @@ export const login = async (
 export const register = async (
   userRequest: UserRequest
 ): Promise<RegisterResponse> => {
+  const path = '/users';
   const httpRequest = httpClient(cienciaArgentinaHost);
-  const path = 'users';
   const { data } = await httpRequest.post<RegisterResponse>(path, userRequest);
   return data;
 };
